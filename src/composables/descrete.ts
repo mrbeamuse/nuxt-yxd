374c9812b40e63f9dc93ae0cfd988a016ee215ea
beams <834483878@qq.com>
2023-09-09 14:09:54 +0800

    feat(custom): 测试接口请求功能

---
+++ src/composables/descrete.ts
@@ -0,0 +1,8 @@
+import { createDiscreteApi } from 'naive-ui'
+
+export const { message, notification, dialog, loadingBar } = createDiscreteApi([
+  'message',
+  'dialog',
+  'notification',
+  'loadingBar'
+])
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1 class="bg-slate-800">欢迎来到周师傅的第一个页面</h1>
+    <img
+      v-for="(item, index) in bannerList"
+      :key="index"
+      :src="item?.url"
+      alt=""
+    />
     <!-- <NButton>hello</NButton>
     <NuxtLink to="/list"> 进入列表页 </NuxtLink>
     <NuxtLink to="/user-admin/111"> 进入管理员 id 为 111 的用户中心 </NuxtLink> -->
@@ -9,6 +15,25 @@
 </template>
 
 <script setup>
-const foo = useFoo()
-console.log(foo.value)
+// const foo = useFoo()
+const testData = ref()
+// onMounted(async () => {
+//   await nextTick()
+//   testRequest()
+// })
+const testRequest = async () => {
+  const res = await useFetch(
+    () =>
+      `https://tpay.xiaokangyouxuan.cn/optimization-pms/advertising/noToken/listPmsAdvertisingList/1`
+  )
+  testData.value = res.data.value.data
+  console.log('index', testData.value)
+}
+
+await testRequest()
+
+const bannerList = computed(() => {
+  console.log('bannerList', testData.value?.advertisingImgs)
+  return testData.value?.advertisingImgs
+})
 </script>
